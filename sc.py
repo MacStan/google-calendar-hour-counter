5905ae77e764a101d32c90f6e262c8c3aec84d2a
Maciej Staniuk <maciej.staniuk@gmail.com>
2018-08-09 23:05:52 +0200

    Added worked hours counting

--- sc.py
+++ sc.py
@@ -23,42 +23,63 @@ def main():
 
     #DATE
     
-    dt =datetime.now()
-    start = dt - timedelta(days=dt.weekday())
+    dt = datetime.now()
+    start = dt - timedelta(days=dt.weekday()) 
     end = start + timedelta(days=6)
-    print(start)
-    print(end.isoformat())
-    
-    
-    # Call the Calendar API
-    now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print(now)
+    start = start - timedelta(hours=dt.hour)
+    print('start: ', start)
+    print('end: ', end)
     
     print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='lbjlpmmfi6bvvp3j5lc450a3rk@group.calendar.google.com', # Work OKE 
                                         timeMin=str(start.isoformat()) +"Z",
                                         timeMax=str(end.isoformat()) +"Z",
-                                        maxResults=10, singleEvents=True,
+                                        maxResults=100, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
-    
+
     dates = {}
     if not events:
         print('No upcoming events found.')
     for event in events:
         try:
             if( "You" in event['summary']):
-                #print(event['summary'])
+                print(event['summary'])
+                print()
+                #print(date.year, date.month, date.day, date.hour, date.minute )
                 date = parse(event['start']['dateTime'])
-                print(date.year, date.month, date.day, date.hour, date.minute )
-                dates[ str(date.day) ]  = (date.hour, date.minute )
+                
+                if(date.day not in dates):
+                    dates[date.day] = []
+                    
+                if(date.day in dates):
+                    if( "entered" in event['summary']):
+                        dates[date.day].append( ('S', date.hour, date.minute) )
+                    else:
+                        dates[date.day].append( ('E', date.hour, date.minute) )
         except:
             pass
     
     print(dates)
-    for date, v in dates.items():
-        print( date, v)
+    days = []
+    for date, value in dates.items():
+        try:
+            start = [x for x in value if x[0] == 'S'][0]
+            end = [x for x in value if x[0] == 'E'][0]
+            span = timedelta(hours = end[1], minutes = end[2]) - timedelta(hours = start[1], minutes = start[2])
+            days.append(span)
+            print( date, span )
+        except:
+            pass
+            
+    worktime = timedelta()
+    for each in days:
+        worktime += each
+    
+    worked = worktime.days * 24 + worktime.seconds /3600
+    print(worked)
+        
     
 if __name__ == '__main__':
     main()
